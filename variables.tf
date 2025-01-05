dbfbed69b4eb1650e3526d54bbfc257b579afee2
Blessing Ogbeh <boogbeh@gmail.com>
2025-01-05 03:35:04 -0400

    updated variables.tf

--- variables.tf
+++ variables.tf
@@ -2,6 +2,5 @@
 
 variable "aws_kinesis_stream_name" {
   description = "The name of the Kinesis stream"
-  type        = string
   default     = ["bus-location-stream", "van-location-stream", "weather-stream"]
 }
