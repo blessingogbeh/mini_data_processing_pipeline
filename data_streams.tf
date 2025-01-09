74cbacdc3514a577f9d4c7a0145f77e596685d73
Blessing Ogbeh <boogbeh@gmail.com>
2025-01-09 02:18:51 -0400

    changed to 1 data stream with 3 shards

--- data_streams.tf
+++ data_streams.tf
@@ -1,9 +1,8 @@
 # Create the Kinesis Streams
 
 resource "aws_kinesis_stream" "data_stream" {
-  count            = 3
-  name             = var.aws_kinesis_stream_name[count.index]
-  shard_count      = 1
+  name             = data_stream
+  shard_count      = 3
   retention_period = 24
   
   stream_mode_details {
