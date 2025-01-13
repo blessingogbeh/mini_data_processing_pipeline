b61abc7c0f4cc174a9314dc46a249f4148ac3557
Blessing Ogbeh <boogbeh@gmail.com>
2025-01-13 02:55:00 -0400

    updated dependencies

--- data_processing.tf
+++ data_processing.tf
@@ -8,7 +8,7 @@ locals {
 # Upload Lambda zip to S3
 resource "aws_s3_object" "lambda_processing" {
   bucket = aws_s3_bucket.lambda_bucket.id
-  key    = "package.zip"
+  key    = "lambda_function_dependencies.zip"
   source = local.lambda_processing_function
   etag   = filemd5(local.lambda_processing_function)
 }
@@ -27,7 +27,7 @@ resource "aws_lambda_function" "processing_lambda" {
 
   source_code_hash = filebase64sha256(local.lambda_processing_function)
 
-  runtime = "python3.8"
+  runtime = "python3.11"
 
   environment {
     variables = {
