e70128d04f7393490746edee7829643ab02fd1c4
Blessing Ogbeh <boogbeh@gmail.com>
2025-01-16 11:13:09 -0400

    .

--- kds_stream.tf
+++ kds_stream.tf
@@ -1,3 +1,16 @@
+# Create the Kinesis Streams
+
+resource "aws_kinesis_stream" "data_stream" {
+  name             = "data_stream"
+  shard_count      = 3
+  retention_period = 24
+  
+  stream_mode_details {
+    stream_mode = "PROVISIONED"
+  }
+}
+
+
 # Event source mapping for Lambda triggers
 
 resource "aws_lambda_event_source_mapping" "kinesis_stream_trigger" {
